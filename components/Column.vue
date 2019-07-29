7e8719268b3aa2ca82628b5b0159629b68f5cfc5
Olawale Samuel <olawalestone471@gmail.com>
2019-07-29 13:51:32 -0700

    Link for the images

--- components/Column.vue
+++ components/Column.vue
@@ -2,22 +2,31 @@
 <div>
     <b-row class="flex">
     <b-col cols="8" sm="8">
-      <img href="#" src="~/assets/interface.png" fluid-grow alt="Interface Designs">
+      <a href="#">
+      <img  src="~/assets/interface.png" fluid-grow alt="Interface Designs">
+      </a>
     </b-col>
       
     <b-col cols="4" sm=4>
-      <img href="#" src="~/assets/timeoff.png" fluid-grow alt="Timeoff Management App">
+      <a href="#">
+      <img href="#" src="~/assets/timeoff.png" fluid-grow alt="Timeoff 
+      Management App">
+      </a>
     </b-col>
   </b-row>
 
   <b-row>
     
     <b-col cols="6">
+      <a href="#">
       <img href="#" src="~/assets/brand.png" fluid-grow alt="Branding Designs">
+      </a>
     </b-col>
       
     <b-col cols="6">
+      <a href="#">
       <img href="#" src="~/assets/tapkey.png" fluid-grow alt="Tapkey Smart Lock">
+      </a>
     </b-col>
   </b-row>
 </div>
